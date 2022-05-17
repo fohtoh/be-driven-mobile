2ca11d2028d37d682d43b5b0735de607a675377e
Jeremy Stowell <stowelljr@churchofjesuschrist.org>
2022-05-16 23:10:34 -0600

    Added logout

--- app/screens/AccountScreen.js
+++ app/screens/AccountScreen.js
@@ -3,13 +3,16 @@ import { Text } from 'react-native';
 import Button from '../components/Button';
 import Screen from '../components/Screen';
 import useAuth from "../auth/useAuth";
+import { withAuthenticator } from "aws-amplify-react-native";
+import { Auth } from "aws-amplify";
 
 
 const AccountScreen = () => {
-  const auth = useAuth();
+  // const auth = useAuth();
 
-  const handleLogout = () => {
-    auth.logOut();
+  const handleLogout = async () => {
+    // auth.logOut();
+    await Auth.signOut()
   }
   return (
     <Screen>
